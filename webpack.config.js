0c59bf80ed9dc609ccdfa67fe0b6b44f9d0a08a0
logon <logon.q@gmail.com>
2019-04-03 09:57:01 +0200

    Some configuration to remember

--- webpack.config.js
+++ webpack.config.js
@@ -7,3 +7,26 @@ module.exports = {
     path: path.resolve(__dirname, "dist")
   }
 };
+
+//  "build-dev": "webpack --mode development --devtool source-map", --watch exists
+//  "build-prod": "webpack --mode production" --watch
+
+/* var config = {
+  entry: "./src/main.js",
+  output: {
+    filename: "build.main.js",
+    path: path.resolve(__dirname, "dist")
+  }
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    config.devtool = "source-map";
+  }
+
+  if (argv.mode === "production") {
+    // ...
+  }
+
+  return config;
+}; */
