1d83c8c78f242b0731e74d463c259613ca8dc893
logon <logon.q@gmail.com>
2019-04-04 17:34:26 +0200

    Remove a shitton of old code that does not work or tests.

--- src/stores/store.js
+++ src/stores/store.js
@@ -12,10 +12,11 @@ import { mapStores } from "./MobxUtils";
 
 class GlobalStore {
   constructor() {
+    // After this global store class is instatiated via the constructor function,
     // @observable
-
-    this.substores = mapStores(subStores); // After this global store class is instatiated via the constructor function,
-    const disposer = deepObserve(this.substores, (change, path) => {
+    this.substores = mapStores(subStores);
+    // Observe all values in the store
+    deepObserve(this.substores, (change, path) => {
       if (change.type === "update") {
         let snap = {};
         snap[path] = {};
@@ -24,90 +25,26 @@ class GlobalStore {
         this.pushSnapshotAndSave(snap);
       }
     });
-    /* for (const k in this.substores) {
-      console.log(k);
-      let d = observable(this.substores[k], { deep: true });
-      reaction(() => this.substores[k], (d, reaction) => { console.log("G3", d, reaction); });
-    } */
-    // const obs = observable(this.substores);
-    // this.test = observable.array(subStores, { deep: true });
-    // this.lastSnapshot = _.cloneDeep(this.substores);
-    // var { UndoStore } = this.substores; // you can even deconstruct the state immidiately after instantiation,
-    // reaction(() => this.substores, data => console.log("test"));
-    console.log(this.substores);
-    // autorun(() => { console.log("gg1", this.substores); });
-    // observe(this.substores, change => console.log("GG", change));
-    // const disposer = autorun(reaction => { console.log("gg2", reaction); });
-    // const component = observer(substores => { console.log("GG3"); });
 
-    this.pushSnapshotAndSave = async snapshot => { //= async snapshot =>
+    this.pushSnapshotAndSave = async snapshot => {
       let { UndoStore } = this.substores;
       console.log("push", snapshot);
-      if (snapshot) { // && UiStore.autoSaveDrafts
-        // console.log("TEST:", this.substores);
+      if (snapshot) {
         UndoStore.pushSnapshot(snapshot);
-        // UiStore.displayUndo = true;
         console.log("snapshot saved!", snapshot);
       } else {
         console.log("no snapshot saved!", snapshot);
       }
-      // UndoStore.setLastFullSnapshot(this.substores);
     };
-
-    /*
-    reaction(() => this.test, (test, reaction) => {
-      console.log("TEST");
-      for (const r in reaction.observing) {
-        let react = reaction.observing[r];
-        console.log("REACT test", react.value, react);
-      }
-    });
-
-    reaction(() => this.snapshot, (snapshot, reaction) => {
-      for (const r in reaction.observing) {
-        let react = reaction.observing[r];
-        console.log("REACT snapshot", react.value, react);
-      }
-      this.pushSnapshotAndSave(snapshot);
-      this.lastSnapshot = _.cloneDeep(this.substores);
-    }); // and bind reactions to the global actions below */
-
-    // map the substores to this object
-    // this.pushSnapshotAndSave(this.snapshot);
-
-    /* reaction(
-      () => UndoStore.snapshots.length > 1,
-      bool => (UiStore.displayUndo = bool)
-    ); // or bind reactions to and from the substores */
-    /* let reactList = {};
-
-    for (var subStore in this.substores) {
-      for (var data in this.substores[subStore]) {
-        console.log("KAPPA", subStore, data);
-        reactList[subStore] = reaction(
-          () => this.substores[subStore][data],
-          d => console.log("REACT", subStore, data, d)
-        );
-      }
-    }
-    console.log(reactList); */
-    /* reaction(
-      () => this.substores,
-      substores => console.log("REACT", this.substores.NumberStore)
-    ); */
   }
 
   resetState() {
     let { UndoStore, UiStore } = this.substores; // ColorStore
     let lastSnapshot = UndoStore.lastSnapshot();
     if (lastSnapshot) {
-      // UiStore.autoSaveDrafts = false; // turn off the un-intended side-effect of saving state when
-
       // here is where the entire application state is reset based on the last snapshot, see Snapshot.js
       this.substores = resetSnapshot(lastSnapshot, this.substores);
       UndoStore.popSnapshot();
-
-      // UiStore.autoSaveDrafts = true; // back on
     }
   }
 
@@ -142,23 +79,10 @@ class GlobalStore {
       user.name.toLowerCase().includes(UiStore.searchText.toLowerCase())
     );
   } */
-
-  /* get change() {
-    console.log("react change");
-    return this.substores;
-  }
-
-  // @computed
-  get snapshot() {
-    return snapshotModel(this.substores, this.lastSnapshot);
-  } */
 }
 
 decorate(GlobalStore, {
-  substores: observable,
-  // change: computed,
-  // snapshot: computed,
-  test: observable
+  substores: observable
 });
 
 export default new GlobalStore();
