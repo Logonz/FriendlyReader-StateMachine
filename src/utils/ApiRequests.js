1760eb465372722ada2d0cb5eca88be6b2c324b5
logon <logon.q@gmail.com>
2019-04-04 10:54:30 +0200

    ApiRequests file for SAPIS and KOGSUM

---
+++ src/utils/ApiRequests.js
@@ -0,0 +1,19 @@
+import req from "request";
+
+export function SAPISRequest(data, stillettOptions) {
+  var sendData = { options: stillettOptions + "\tLexicalMetrics()\tSurfaceMetrics()\tStructuralMetrics()",
+    document: data };
+  req.post({
+    headers: { "content-type": "application/json; charset=UTF-8" },
+    url: "https://www.ida.liu.se/projects/scream/services/sapis/service/",
+    body: JSON.stringify(sendData)
+  }, function(error, response, body) {
+    if (error) {
+      console.error(error);
+    }
+    body = JSON.parse(body);
+
+    console.log(body);
+    return body;
+  });
+}
