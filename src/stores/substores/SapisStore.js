6d3fa3ce56d9510296cb2892256070a26fc3be9d
logon <logon.q@gmail.com>
2019-04-04 13:17:56 +0200

    Fixed request

--- src/stores/substores/SapisStore.js
+++ src/stores/substores/SapisStore.js
@@ -6,10 +6,11 @@ import { SAPISRequest } from "../../utils/ApiRequests";
 export default class SapisStore {
   constructor() {
     this.requesting = false;
+    this.lastrequestdate = Date.now();
     // @observable
     this.data = {};
 
-    reaction(() => this.data, data => console.log("New SAPIS data", this.data));
+    reaction(() => this.data, data => console.log("SAPIS: New data", this.data));
 
     // Does not work due to not being an observable
     // reaction(() => this.requesting, req => console.log("Requesting", this.requesting));
@@ -17,10 +18,18 @@ export default class SapisStore {
 
   // @action
   analyzeText(data, stillettOptions) {
-    this.requesting = true;
-    if (!stillettOptions) { stillettOptions = "Feedback(-svo -pass2act -prox -quoteInv -split)"; }
-    // Function(data) for callback, this.data modifies the state after the data is recived
-    SAPISRequest(data, function(data) { this.data = data; this.requesting = false; }.bind(this), stillettOptions);
+    if (!this.requesting) {
+      this.requesting = true;
+      if (!stillettOptions) { stillettOptions = "Feedback(-svo -pass2act -prox -quoteInv -split)"; }
+      // Function(data) for callback, this.data modifies the state after the data is recived
+      SAPISRequest(data, function(data) {
+        this.data = data;
+        this.requesting = false;
+        this.lastrequestdate = Date.now();
+      }.bind(this), stillettOptions);
+      return true;
+    }
+    return false;
   }
 };
 
