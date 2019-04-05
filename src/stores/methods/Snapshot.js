1702a242fed17cbb3b39d3c06c425e32b49e69e1
logon <logon.q@gmail.com>
2019-04-05 16:29:21 +0200

    Cleaned up the reset function

--- src/stores/methods/Snapshot.js
+++ src/stores/methods/Snapshot.js
@@ -1,12 +1,9 @@
 
 export function resetSnapshot(snapshot, state) {
-  console.log("resetSnapshot", snapshot, state);
   for (var store in snapshot) {
     for (var data in snapshot[store]) {
-      console.log("SNAP", store, data, snapshot[store]);
       state[store][data] = snapshot[store][data];
     }
   }
-
   return state;
 }
