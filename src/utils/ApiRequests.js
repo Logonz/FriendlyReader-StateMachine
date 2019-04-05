5d5db5972450b1c9c82baf98d303c6a0aed42c60
logon <logon.q@gmail.com>
2019-04-05 16:31:00 +0200

    Changed lib from request to https instead

--- src/utils/ApiRequests.js
+++ src/utils/ApiRequests.js
@@ -1,9 +1,47 @@
-import req from "request";
+const https = require("https");
 
 export function SAPISRequest(data, callback, stillettOptions) {
+  var sendData = { options: stillettOptions, // stillettOptions = "Feedback(-svo -pass2act -prox -quoteInv -split)" + "\tLexicalMetrics()\tSurfaceMetrics()\tStructuralMetrics()";
+    document: data };
+  sendData = JSON.stringify(sendData);
+  var options = {
+    path: "https://www.ida.liu.se/projects/scream/services/sapis/service/",
+    method: "POST",
+    headers: {
+      "content-type": "application/json; charset=UTF-8",
+      "Content-Length": Buffer.byteLength(sendData)
+    }
+  };
+  console.log("Sending SAPIS request");
+  var req = https.request(options, (res) => {
+    res.setEncoding("utf8");
+    console.log("SAPIS REQUEST:\nStatusCode:", res.statusCode, "\nHeaders:", res.headers);
+    var data = [];
+
+    res.on("data", function(chunk) {
+      data.push(chunk);
+    }).on("end", function() {
+      let body = JSON.parse(data[0]);
+      console.log("REQUEST COMPLETE:\n", body);
+      callback(body);
+    });
+  });
+
+  req.on("error", (e) => {
+    console.error("SAPIS REQUEST ERROR:\n", e);
+  });
+
+  req.write(sendData);
+  req.end();
+}
+
+// OLD CODE
+// import { post } from "request";
+
+/* export function SAPISRequest(data, callback, stillettOptions) {
   var sendData = { options: stillettOptions + "\tLexicalMetrics()\tSurfaceMetrics()\tStructuralMetrics()",
     document: data };
-  req.post({
+  post({
     headers: { "content-type": "application/json; charset=UTF-8" },
     url: "https://www.ida.liu.se/projects/scream/services/sapis/service/",
     body: JSON.stringify(sendData)
@@ -16,4 +54,4 @@ export function SAPISRequest(data, callback, stillettOptions) {
     console.log(body);
     callback(body);
   });
-}
+} */
