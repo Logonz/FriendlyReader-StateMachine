3ef799318a84d70ee579f460cb248399dd90b414
logon <logon.q@gmail.com>
2019-04-08 13:26:31 +0200

    Changed how the object is reset to be able to handle the array paths (WordState/wordsnap)

--- src/stores/methods/Snapshot.js
+++ src/stores/methods/Snapshot.js
@@ -1,9 +1,43 @@
 
+// Source: https://hackernoon.com/accessing-nested-objects-in-javascript-f02f1bd6387f
+const getNestedObject = (nestedObj, pathArr) => {
+  return pathArr.reduce((obj, key) =>
+    (obj && obj[key] !== "undefined") ? obj[key] : undefined, nestedObj);
+};
+
 export function resetSnapshot(snapshot, state) {
   for (var store in snapshot) {
+    let pathArray = store.split("/");
+    console.log(snapshot, pathArray);
+    var data = getNestedObject(state, pathArray);
+
+    for (var key in snapshot[store]) {
+      data[key] = snapshot[store][key];
+    }
+    /*
+
+    let path = store.split("/");
+    let dataa = state;
+    for (let depth = 0; depth < path.length; depth += 1) {
+      console.log(dataa, depth, path[depth], path.length);
+
+      if (depth === path.length - 1) {
+        console.log("inside");
+        for (var data in snapshot[store]) {
+          console.log(dataa, data);
+          dataa[depth][data] = snapshot[store][data];
+          // state[store][data] = snapshot[store][data];
+        }
+      } else {
+        console.log("deeper");
+        dataa = getNextDepth(dataa, path[depth]);
+      }
+    }
+    /*
+    console.log(dataa, dataa.length);
     for (var data in snapshot[store]) {
       state[store][data] = snapshot[store][data];
-    }
+    } */
   }
   return state;
 }
