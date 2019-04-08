78cc324735b312dd109653e6181893b4955a6910
logon <logon.q@gmail.com>
2019-04-08 15:24:07 +0200

    More helper functions exposed to the user.

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 var database = require("./database.js");
+var testing = require("./utils/automatedtesting.js");
+var sapis = require("./utils/ApiRequests.js");
 
 module.exports = {
   initialize: function() {
     return database.getStore();
-  }
+  },
+  runTests: function(GlobalStore) {
+    testing.wordTest(GlobalStore);
+  },
+  Mobx: database.getMobx(),
+  API: sapis
 };
 
 console.log("Loaded main.js");
