53f04407e14967cd554814574e73e02df5729b00
logon <logon.q@gmail.com>
2019-04-16 16:28:55 +0200

    Added utils for crazyToken

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 var database = require("./database.js");
 var testing = require("./utils/automatedtesting.js");
 var sapis = require("./utils/ApiRequests.js");
+var utils = require("./utils/utils.js");
 
 module.exports = {
   initialize: function() {
@@ -10,7 +11,8 @@ module.exports = {
     testing.wordTest(GlobalStore);
   },
   Mobx: database.getMobx(),
-  API: sapis
+  API: sapis,
+  utils: utils
 };
 
 console.log("Loaded main.js");
