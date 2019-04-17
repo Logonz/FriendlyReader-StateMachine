ae89e14a1d6b9a405305c6aecfa94e58671dc14a
logon <logon.q@gmail.com>
2019-04-17 11:18:07 +0200

    Renamed the crazyTokens to tokenizeText to fit the naming convention better

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,5 @@
 
-export function crazyTokens (string) { // Skapar en array med tokens (alltså alla möjliga ord tecken)
+export function tokenizeText (string) { // Skapar en array med tokens (alltså alla möjliga ord tecken)
   var regex = /<[\w\s!"#$%&'()*+,\-./:;=?@[\\\]^_`{|}~”\u00C0-\u017F]+>|(&\w+;)|(\d+)|(\w+-)|([\w\u00C0-\u017F]+)|([\s.,!?()[\]:"”;-])/g;
   var stringArray = string.match(regex);
   return stringArray;
